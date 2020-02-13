efae450206ea94ffaf3b17aa58de9af170a26003
动辄 <ktjay520@qq.com>
2020-02-13 17:27:27 +0800

    update includes/classes/class.items.php.

--- includes/classes/class.items.php
+++ includes/classes/class.items.php
@@ -1118,7 +1118,7 @@ Class WoW_Items {
     }
 
     public function GetItemSource($entry) {
-        $source_info = DB::WoW()->selectRow("SELECT `source`, `areaKey` FROM `DBPREFIX_sources` WHERE `item` = %d", $entry);
+        $source_info = DB::WoW()->selectRow("SELECT `source`, `areaKey` FROM `DBPREFIX_source` WHERE `item` = %d", $entry);
         if(!$source_info) {
             return false;
         }
